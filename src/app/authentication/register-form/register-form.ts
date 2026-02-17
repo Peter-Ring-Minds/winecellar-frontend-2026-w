36cc2db7785703a10f747b039f522e0ce854f00c
LauritsGude <laurits.gude@it-minds.dk>
2026-02-17 15:07:32 +0100

    fix: Added some error handling to the Register page

--- src/app/authentication/register-form/register-form.ts
+++ src/app/authentication/register-form/register-form.ts
@@ -7,7 +7,13 @@ import {
   Output,
   signal,
 } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { email } from '@angular/forms/signals';
 
 @Component({
@@ -21,24 +27,48 @@ export class RegisterForm {
   public buttonText = input('');
   public authSubmit = output<{ email: string; password: string }>();
 
-  registerForm = new FormGroup({
-    email: new FormControl('', { validators: [Validators.required], nonNullable: true }),
-    password: new FormControl('', { validators: [Validators.required], nonNullable: true }),
-    repeatPassword: new FormControl('', { validators: [Validators.required], nonNullable: true }),
-  });
+  registerForm = new FormGroup(
+    {
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.email],
+        nonNullable: true,
+      }),
+      password: new FormControl('', { validators: [Validators.required], nonNullable: true }),
+      repeatPassword: new FormControl('', { validators: [Validators.required], nonNullable: true }),
+    },
+    { validators: passWordMatchValidator },
+  );
 
   protected readonly passwordDoesntMatch = signal(false);
 
+  showErrors = signal(false);
+
   onSubmit() {
+    this.showErrors.set(false);
+    this.passwordDoesntMatch.set(false);
+
+    this.registerForm.markAllAsTouched();
     if (!this.registerForm.valid) {
+      this.showErrors.set(true);
       return;
     }
     if (this.registerForm.value.password !== this.registerForm.value.repeatPassword) {
       this.passwordDoesntMatch.set(true);
+      this.showErrors.set(true);
       return;
     }
-    this.passwordDoesntMatch.set(false);
 
     this.authSubmit.emit(this.registerForm.getRawValue());
   }
 }
+
+const passWordMatchValidator = (control: AbstractControl) => {
+  const passwordControl = control.get('password');
+  const repeatPasswordControl = control.get('repeatPassword');
+
+  if (passwordControl?.value !== repeatPasswordControl?.value) {
+    return { passwordMismatch: true };
+  }
+
+  return null;
+};
